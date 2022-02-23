f199ceef0ead7123c674b79a5ef8d8957e077351
mohamed abdelhakim <mohamed.hope18@gmail.com>
2022-02-23 21:10:21 +0200

    Add project files.

---
+++ ResalaCharityProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ResalaCharityProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
